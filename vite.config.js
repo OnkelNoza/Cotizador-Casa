d7322c6584dbe31b423adbbd845e8737ed5fb582
OnkelNoza <franquitodeigartua@hotmail.com>
2025-12-04 02:28:32 -0300

    third commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://OnkelNoza.gethub.io/Cotizador-Casa",
+  base: "https://OnkelNoza.github.io/Cotizador-Casa",
 })
